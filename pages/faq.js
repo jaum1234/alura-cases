b91f24f02490e805597fa03a0230ad3dcf6e5ebe
jaum1234 <joaovitorsouzacoura@gmail.com>
2022-02-12 07:52:15 -0300

    trabalhando com arquivos estaticos; utilizando funçao getStaticProps()

--- pages/faq.js
+++ pages/faq.js
@@ -1,12 +1,49 @@
+//import { useEffect, useState } from "react";
 import Link from "../src/components/Link";
 
-const FAQ = () => {
+export async function getStaticProps() {
+    const apiUrl = 'https://gist.githubusercontent.com/omariosouto/0ceab54bdd8182cbd1a4549d32945c1a/raw/578ad1e8e5296fa048e3e7ff6b317f7497b31ad9/alura-cases-faq.json'
+        
+    const faq = await fetch(apiUrl)
+        .then(res => res.json())
+        .then(res => res);
+
+    return {
+      props: {
+        faq
+      }, 
+    }
+  }
+
+const FAQ = ({ faq }) => {
+
+    //const [faq, setFaq] = useState([]);
+//
+    //useEffect(() => {
+    //    const apiUrl = 'https://gist.githubusercontent.com/omariosouto/0ceab54bdd8182cbd1a4549d32945c1a/raw/578ad1e8e5296fa048e3e7ff6b317f7497b31ad9/alura-cases-faq.json'
+    //    
+    //    fetch(apiUrl)
+    //        .then(res => res.json())
+    //        .then(res => setFaq(res));
+    //}, []);
+    
+    console.log(faq);
     return(
         <div>
             <h1>Alura Cases - Pagina de Perguntas FAQ</h1>
             <Link url='/home'>
                 Ir para a Home
             </Link>
+            <ul>
+                { faq.map(item => (
+                    <li>
+                        <article>
+                            <h2>{ item.question }</h2>
+                            <p>{ item.answer }</p>
+                        </article>
+                    </li>
+                )) }
+            </ul>
         </div>
     );
 }
--- pages/home.js
+++ pages/home.js
@@ -1,32 +1,36 @@
-import Link from "../src/components/Link";
+import HomeScreen from "../src/components/screens/HomeScreen";
 
-
-const Title = ({children, as}) => {
-    const Tag = as;
-    return(
-        <>
-            <Tag>    
-                { children }
-            </Tag>
-            <style jsx>{`
-                ${Tag} {
-                    color: red;
-                    font-weight: bold;
-                }
-            `}</style>
-        </>
-    );
-}
-
-const HomePage = () => {
-    return(
-        <div>
-            <Title as='h1'>Alura Cases</Title>
-            <Link url='/faq'>
-                Ir para o FAQ
-            </Link>
-        </div>
-    )
-}
-
-export default HomePage;
+export default HomeScreen;
+//import Link from "../src/components/Link";
+//import Footer from "../src/components/shared/Footer";
+//
+//const Title = ({children, as}) => {
+//    const Tag = as;
+//    return(
+//        <>
+//            <Tag>    
+//                { children }
+//            </Tag>
+//            <style jsx>{`
+//                ${Tag} {
+//                    color: red;
+//                    font-weight: bold;
+//                }
+//            `}</style>
+//        </>
+//    );
+//}
+//
+//const HomePage = () => {
+//    return(
+//        <div>
+//            <Title as='h1'>Alura Cases</Title>
+//            <Link url='/faq'>
+//                Ir para o FAQ
+//            </Link>
+//            <Footer/>
+//        </div>
+//    )
+//}
+//
+//export default HomePage;
